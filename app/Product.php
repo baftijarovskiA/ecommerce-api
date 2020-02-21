84cfaf4f96e3bca9d21e8ab7a33237b6c74404c3
Albin Baftijarovski <baftijarovski.a@outlook.com>
2020-02-21 23:13:22 +0100

    final changes

--- app/Product.php
+++ app/Product.php
@@ -12,4 +12,9 @@ class Product extends Model
     public function category(){
         return $this->belongsTo('App\Category');
     }
+
+    public function products()
+    {
+        return $this->belongsToMany('App\Order', 'order_product');
+    }
 }
